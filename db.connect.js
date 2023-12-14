22fe852cd1eed0fdbad2310119b74942ab6821c3
Arun1only1 <arun1nly1@gmail.com>
2023-12-14 17:30:42 +0545

    backend setup

---
+++ db.connect.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const dbName = process.env.DB_NAME;
+const dbPass = encodeURIComponent(process.env.DB_PASSWORD);
+const dbUserName = process.env.DB_USERNAME;
+
+console.log(dbName, dbPass, dbUserName);
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${dbUserName}:${dbPass}@school.b6qkdnb.mongodb.net/${dbName}?retryWrites=true&w=majority`
+    );
+
+    console.log("DB connection established...");
+  } catch (error) {
+    console.log("DB connection failed...");
+    console.log(error.message);
+  }
+};
