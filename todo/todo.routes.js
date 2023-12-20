8fc7503122ccd853d48decad22fcc1a6a5e66cd9
Arun1only1 <arun1nly1@gmail.com>
2023-12-20 17:11:44 +0545

    todo routes

--- todo/todo.routes.js
+++ todo/todo.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
-import { todoValidationSchema } from "./todo.validation.js";
+import { validateAccessToken } from "../middlewares/authentication.middleware.js";
+import { checkMongoIdValidity } from "../utils/mongo.id.validity.js";
 import { Todo } from "./todo.model.js";
-import { extractAccessTokenFromHeaders } from "../utils/token.from.headers.js";
-import jwt from "jsonwebtoken";
-import { User } from "../user/user.model.js";
+import { todoValidationSchema } from "./todo.validation.js";
 
 const router = express.Router();
 
 router.post(
   "/todo/add",
+  validateAccessToken,
   async (req, res, next) => {
     const newTodo = req.body;
 
@@ -22,18 +22,7 @@ router.post(
   },
   async (req, res) => {
     const newTodo = req.body;
-
-    const token = extractAccessTokenFromHeaders(req.headers.authorization);
-
-    // decrypt the token
-    const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-    // find user with email from payload
-    const user = await User.findOne({ email: payload.email });
-
-    if (!user) {
-      return res.status(401).send({ message: "Unauthorised." });
-    }
+    const user = req.userDetails;
 
     newTodo.userId = user._id;
 
@@ -42,5 +31,78 @@ router.post(
     return res.status(201).send({ message: "Todo is added successfully." });
   }
 );
+
+router.delete("/todo/delete/:id", validateAccessToken, async (req, res) => {
+  // extract id from req.params
+  const todoId = req.params.id;
+
+  // check for mongo id validity
+  const isValidMongoId = checkMongoIdValidity(todoId);
+
+  // if not valid mongo id, throw error
+  if (!isValidMongoId) {
+    return res.status(400).send({ message: "Invalid mongo id." });
+  }
+
+  // check if todo with that id exists
+  const todo = await Todo.findOne({ _id: todoId });
+
+  // if not todo, throw error
+  if (!todo) {
+    return res.status(404).send({ message: "Todo does not exist." });
+  }
+
+  // check if loggedIn user is owner of that todo
+  const tokenUserId = req.userDetails._id;
+  const todoOwnerId = todo.userId;
+
+  const isOwnerOfTodo = todoOwnerId.equals(tokenUserId);
+
+  // if not owner ,throw error
+  if (!isOwnerOfTodo) {
+    return res.status(403).send({ message: "You are not owner of this todo." });
+  }
+
+  // delete todo
+  await Todo.deleteOne({ _id: todoId });
+  // send appropriate response
+
+  return res.status(200).send({ message: "Todo is deleted successfully." });
+});
+
+router.get("/todo/details/:id", validateAccessToken, async (req, res) => {
+  // extract id from req.params
+  const todoId = req.params.id;
+
+  // check for mongo id validity
+  const isValidMongoId = checkMongoIdValidity(todoId);
+
+  // if not valid mongo id, throw error
+  if (!isValidMongoId) {
+    return res.status(400).send({ message: "Invalid mongo id." });
+  }
+
+  // check if todo exists with provided todoId
+  const todo = await Todo.findOne({ _id: todoId });
+
+  // if todo not found, throw error
+  if (!todo) {
+    return res.status(404).send({ message: "Todo does not exist." });
+  }
+
+  // check for todo ownership
+  const todoOwnerId = todo.userId;
+  const loggedInUserId = req.userDetails._id;
+
+  const isOwnerOfTodo = todoOwnerId.equals(loggedInUserId);
+
+  if (!isOwnerOfTodo) {
+    return res.status(403).send({ message: "You are not owner of this todo." });
+  }
+
+  todo.userId = undefined;
+
+  return res.status(200).send(todo);
+});
 
 export default router;
