7da1b4e762c5e282c9ec5b5c9388ffd42e0aebe2
Arun1only1 <arun1nly1@gmail.com>
2024-01-08 17:17:01 +0545

    cors added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { connectDB } from "./db.connect.js";
 import todoRoutes from "./todo/todo.routes.js";
 import userRoutes from "./user/user.routes.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // connect db
 connectDB();
